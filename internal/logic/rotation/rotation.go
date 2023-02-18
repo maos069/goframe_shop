58bfcba2854ad5a7c4c2642c0220d01d14f6fcfc
mao <maoshuai069@gmail.com>
2023-02-18 13:09:30 +0800

    轮播图更新

--- internal/logic/rotation/rotation.go
+++ internal/logic/rotation/rotation.go
@@ -22,8 +22,9 @@ func New() *sRotation {
 }
 
 func (s *sRotation) Create(ctx context.Context, in model.RotationCreateInput) (out model.RotationCreateOutput, err error) {
-	if err = ghtml.SpecialCharsMapOrStruct(in); err != nil {
-		return out, err
+	// 不允许HTML代码
+	if err := ghtml.SpecialCharsMapOrStruct(in); err != nil {
+		return out,err
 	}
 	lastInsertID, err := dao.RotationInfo.Ctx(ctx).Data(in).InsertAndGetId()
 	if err != nil {
@@ -35,11 +36,29 @@ func (s *sRotation) Create(ctx context.Context, in model.RotationCreateInput) (o
 // Delete 删除
 func (s *sRotation) Delete(ctx context.Context, id uint) error {
 	return dao.RotationInfo.Transaction(ctx, func(ctx context.Context, tx gdb.TX) error {
+		
 		// 删除轮播图
 		_, err := dao.RotationInfo.Ctx(ctx).Where(g.Map{
 			dao.RotationInfo.Columns().Id: id,
 		}).Delete()
 		//可以使用.Unscoped().Delete()忽略delete_at时间属性，强制删除
+		return err
+	})
+}
+
+// Update 修改
+func (s *sRotation) Update(ctx context.Context, in model.RotationUpdateInput) error {
+	return dao.RotationInfo.Transaction(ctx, func(ctx context.Context, tx gdb.TX) error {
+		// 不允许HTML代码
+		if err := ghtml.SpecialCharsMapOrStruct(in); err != nil {
+			return err
+		}
+		_, err := dao.RotationInfo.
+			Ctx(ctx).
+			Data(in).
+			FieldsEx(dao.RotationInfo.Columns().Id).
+			Where(dao.RotationInfo.Columns().Id, in.Id).
+			Update()
 		return err
 	})
 }
--- internal/model/rotation.go
+++ internal/model/rotation.go
@@ -2,15 +2,21 @@ package model
 
 //RotationCreateUpdateBase 创建/修改Rotation基类
 type RotationCreateUpdateBase struct {
-	PicUrl    string      `json:"picUrl"    description:"轮播图片"`
-	Link      string      `json:"link"      description:"跳转链接"`
-	Sort      int         `json:"sort"      description:"排序字段"`
+	PicUrl string `json:"picUrl"    description:"轮播图片"`
+	Link   string `json:"link"      description:"跳转链接"`
+	Sort   int    `json:"sort"      description:"排序字段"`
 }
 
-type RotationCreateInput struct{
+type RotationCreateInput struct {
 	RotationCreateUpdateBase
 }
 
-type RotationCreateOutput struct{
+type RotationCreateOutput struct {
 	RotationId uint
+}
+
+// RotationUpdateInput 修改内容
+type RotationUpdateInput struct {
+	RotationCreateUpdateBase
+	Id uint
 }
